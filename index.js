ffff478b6bc25670cafce20407c752db1c133924
Vinit Joshi <vinitjoshi.2345@gmail.com>
2021-10-01 14:23:16 +0530

    add nodemailer ,setup config

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
 // const dotenv = require("dotenv");
 // dotenv.config();
 const port = process.env.PORT || 3000;
@@ -8,7 +9,44 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ success: "Hello World" });
 });
+let transport = {
+  host: "smtp.mailtrap.io",
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS,
+  },
+};
 
+let transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("All works fine");
+  }
+});
+app.post("/send", (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  let mail = {
+    from: email,
+    to: email,
+    subject: "Contact form request",
+
+    html: message,
+  };
+  console.log(mail);
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({ msg: "fail" });
+    } else {
+      res.json({ msg: "success" });
+    }
+  });
+});
 app.listen(port, () => {
   console.log(`server is running on port 3000 `);
 });
